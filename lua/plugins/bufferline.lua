dc9bb529b266223d452b7e4c51c95ed75beffe3c
moyiz <8603313+moyiz@users.noreply.github.com>
2024-03-10 19:04:20 +0200

    Add shortcuts to dashboard

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,5 +2,9 @@ return {
   'akinsho/bufferline.nvim',
   version = '*',
   dependencies = 'nvim-tree/nvim-web-devicons',
-  opts = {},
+  opts = {
+    -- options = {
+    --   mode = 'buffers',
+    -- },
+  },
 }
