bb210b3997227f8ed76689e646d069a37bd457b5
moyiz <8603313+moyiz@users.noreply.github.com>
2024-04-04 01:23:05 +0300

    keymap: windows selection, chagne bn bp keys

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,13 +3,33 @@
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set(
+  "n",
+  "[d",
+  vim.diagnostic.goto_prev,
+  { desc = "Go to previous [D]iagnostic message" }
+)
+vim.keymap.set(
+  "n",
+  "]d",
+  vim.diagnostic.goto_next,
+  { desc = "Go to next [D]iagnostic message" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>e",
+  vim.diagnostic.open_float,
+  { desc = "Show diagnostic [E]rror messages" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>q",
+  vim.diagnostic.setloclist,
+  { desc = "Open diagnostic [Q]uickfix list" }
+)
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -17,25 +37,41 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set(
+  "t",
+  "<Esc><Esc>",
+  "<C-\\><C-n>",
+  { desc = "Exit terminal mode" }
+)
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set("n", "<leader>n", ":bn<cr>", { desc = "[N]ext buffer" })
+vim.keymap.set("n", "<leader>p", ":bp<cr>", { desc = "[P]revious buffer" })
 
-vim.keymap.set('n', '<leader>b<right>', ':bn<cr>', { desc = 'Move focus to next tab' })
-vim.keymap.set('n', '<leader>b<left>', ':bp<cr>', { desc = 'Move focus to previous tab' })
+vim.keymap.set(
+  "n",
+  "<leader><left>",
+  "<C-w>h",
+  { desc = "Move focus to left window", remap = true }
+)
+vim.keymap.set(
+  "n",
+  "<leader><down>",
+  "<C-w>j",
+  { desc = "Move focus to lower window", remap = true }
+)
+vim.keymap.set(
+  "n",
+  "<leader><up>",
+  "<C-w>k",
+  { desc = "Move focus to upper window", remap = true }
+)
+vim.keymap.set(
+  "n",
+  "<leader><right>",
+  "<C-w>l",
+  { desc = "Move focus to right Window", remap = true }
+)
 
 -- [[ Basic Autocommands ]]
 --  See :help lua-guide-autocommands
@@ -43,9 +79,12 @@ vim.keymap.set('n', '<leader>b<left>', ':bp<cr>', { desc = 'Move focus to previo
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup(
+    "kickstart-highlight-yank",
+    { clear = true }
+  ),
   callback = function()
     vim.highlight.on_yank()
   end,
@@ -54,10 +93,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- Restore last cursor position in opened buffers
 -- local lastplace = vim.api.nvim_create_augroup('LastPlace', {})
 -- vim.api.nvim_clear_autocmds { group = lastplace }
-vim.api.nvim_create_autocmd('BufReadPost', {
-  group = vim.api.nvim_create_augroup('LastPlace', {}),
-  pattern = { '*' },
-  desc = 'remember last cursor place',
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = vim.api.nvim_create_augroup("LastPlace", {}),
+  pattern = { "*" },
+  desc = "remember last cursor place",
   callback = function()
     local mark = vim.api.nvim_buf_get_mark(0, '"')
     local lcount = vim.api.nvim_buf_line_count(0)
