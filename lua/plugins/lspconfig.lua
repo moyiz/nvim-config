7dfc12e2a6fa154209dd0988b068c5912adb83a8
moyiz <8603313+moyiz@users.noreply.github.com>
2024-04-08 18:13:29 +0300

    Add lsp signature, bashls and clangls

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,23 +1,42 @@
 return {
-  { -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    lazy = true,
-    -- event = { 'BufReadPre', 'BufNewFile' },
-    event = { 'BufReadPost' },
+  {
+    "neovim/nvim-lspconfig",
+    -- lazy = true,
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-      -- Automatically install LSPs and related tools to stdpath for neovim
       {
-        'williamboman/mason.nvim',
+        "williamboman/mason.nvim",
         lazy = true,
-        cmd = { 'Mason', 'MasonInstall', 'MasonInstallAll', 'MasonUninstall', 'MasonUninstallAll', 'MasonLog' },
+        cmd = {
+          "Mason",
+          "MasonInstall",
+          "MasonInstallAll",
+          "MasonUninstall",
+          "MasonUninstallAll",
+          "MasonLog",
+        },
       },
-      'williamboman/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
 
-      -- Useful status updates for LSP.
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
-      { 'towolf/vim-helm', ft = 'helm' },
+      -- UI for notifications and LSP progress messages
+      { "j-hui/fidget.nvim", opts = {} },
+
+      { "towolf/vim-helm", ft = "helm" },
+
+      {
+        "ray-x/lsp_signature.nvim",
+        event = "VeryLazy",
+        opts = {
+          bind = true,
+          doc_lines = 25,
+          handler_opts = {
+            border = "rounded",
+          },
+          transparency = 90,
+          floating_window_above_cur_line = false,
+        },
+      },
     },
     config = function()
       -- Brief Aside: **What is LSP?**
@@ -49,59 +68,87 @@ return {
       --    That is to say, every time a new file is opened that is associated with
       --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
       --    function will be executed to configure the current buffer
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup(
+          "kickstart-lsp-attach",
+          { clear = true }
+        ),
         callback = function(event)
-          -- NOTE: Remember that lua is a real programming language, and as such it is possible
-          -- to define small helper and utility functions so you don't have to repeat yourself
-          -- many times.
-          --
           -- In this case, we create a function that lets us more easily define mappings specific
           -- for LSP related items. It sets the mode, buffer and description for us each time.
           local map = function(keys, func, desc)
-            vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+            vim.keymap.set(
+              "n",
+              keys,
+              func,
+              { buffer = event.buf, desc = "LSP: " .. desc }
+            )
           end
 
           -- Jump to the definition of the word under your cursor.
           --  This is where a variable was first declared, or where a function is defined, etc.
           --  To jump back, press <C-T>.
-          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+          map(
+            "gd",
+            require("telescope.builtin").lsp_definitions,
+            "[G]oto [D]efinition"
+          )
+          map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
           -- Find references for the word under your cursor.
-          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+          map(
+            "gr",
+            require("telescope.builtin").lsp_references,
+            "[G]oto [R]eferences"
+          )
 
           -- Jump to the implementation of the word under your cursor.
           --  Useful when your language has ways of declaring types without an actual implementation.
-          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          map(
+            "gI",
+            require("telescope.builtin").lsp_implementations,
+            "[G]oto [I]mplementation"
+          )
 
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
           --  the definition of its *type*, not where it was *defined*.
-          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+          map(
+            "<leader>D",
+            require("telescope.builtin").lsp_type_definitions,
+            "Type [D]efinition"
+          )
 
           -- Rename the variable under your cursor
           --  Most Language Servers support renaming across files, etc.
-          map('<leader>cr', vim.lsp.buf.rename, '[R]ename')
+          map("<leader>cr", vim.lsp.buf.rename, "[R]ename")
 
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
-          map('<leader>ca', vim.lsp.buf.code_action, '[A]ction')
+          map("<leader>ca", vim.lsp.buf.code_action, "[A]ction")
 
           -- Format buffer
-          map('<leader>cf', vim.lsp.buf.format, '[F]ormat')
+          map("<leader>cf", vim.lsp.buf.format, "[F]ormat")
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
-          map('<leader>csd', require('telescope.builtin').lsp_document_symbols, '[D]ocument')
+          map(
+            "<leader>csd",
+            require("telescope.builtin").lsp_document_symbols,
+            "[D]ocument"
+          )
 
           -- Fuzzy find all the symbols in your current workspace
           --  Similar to document symbols, except searches over your whole project.
-          map('<leader>csw', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace')
+          map(
+            "<leader>csw",
+            require("telescope.builtin").lsp_dynamic_workspace_symbols,
+            "[W]orkspace"
+          )
 
           -- Opens a popup that displays documentation about the word under your cursor
           --  See `:help K` for why this keymap
-          map('K', vim.lsp.buf.hover, 'Hover Documentation')
+          map("K", vim.lsp.buf.hover, "Hover Documentation")
 
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
@@ -109,13 +156,15 @@ return {
           --
           -- When you move your cursor, the highlights will be cleared (the second autocommand).
           local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client.server_capabilities.documentHighlightProvider then
-            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+          if
+            client and client.server_capabilities.documentHighlightProvider
+          then
+            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
               buffer = event.buf,
               callback = vim.lsp.buf.document_highlight,
             })
 
-            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
               buffer = event.buf,
               callback = vim.lsp.buf.clear_references,
             })
@@ -128,7 +177,11 @@ return {
       --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
       --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+      capabilities = vim.tbl_deep_extend(
+        "force",
+        capabilities,
+        require("cmp_nvim_lsp").default_capabilities()
+      )
 
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -140,9 +193,12 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        bashls = {},
+        yamlls = {},
+        clangd = {},
         gopls = {},
         pyright = {},
+        -- pylyzer = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -159,27 +215,27 @@ return {
           -- capabilities = {},
           settings = {
             Lua = {
-              runtime = { version = 'LuaJIT' },
+              runtime = { version = "LuaJIT" },
               workspace = {
                 checkThirdParty = false,
                 -- Tells lua_ls where to find all the Lua files that you have loaded
                 -- for your neovim configuration.
-                library = {
-                  '${3rd}/luv/library',
-                  unpack(vim.api.nvim_get_runtime_file('', true)),
-                },
+                -- library = {
+                --   "${3rd}/luv/library",
+                --   unpack(vim.api.nvim_get_runtime_file("", true)),
+                -- },
                 -- If lua_ls is really slow on your computer, you can try this instead:
-                -- library = { vim.env.VIMRUNTIME },
+                library = { vim.env.VIMRUNTIME },
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = { disable = { "missing-fields" } },
             },
           },
         },
         helm_ls = {
-          ['helm-ls'] = {
+          ["helm-ls"] = {
             yamlls = {
-              path = 'yaml-language-server',
+              path = "yaml-language-server",
             },
           },
         },
@@ -191,28 +247,35 @@ return {
       --    :Mason
       --
       --  You can press `g?` for help in this menu
-      require('mason').setup()
+      require("mason").setup()
 
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format lua code
+        "stylua", -- Used to format lua code
       })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+      require("mason-tool-installer").setup {
+        ensure_installed = ensure_installed,
+      }
 
-      require('mason-lspconfig').setup {
+      require("mason-lspconfig").setup {
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
-            require('lspconfig')[server_name].setup {
+            require("lspconfig")[server_name].setup {
               cmd = server.cmd,
               settings = server.settings,
               filetypes = server.filetypes,
               -- This handles overriding only values explicitly passed
               -- by the server configuration above. Useful when disabling
               -- certain features of an LSP (for example, turning off formatting for tsserver)
-              capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {}),
+              capabilities = vim.tbl_deep_extend(
+                "force",
+                {},
+                capabilities,
+                server.capabilities or {}
+              ),
             }
           end,
         },
