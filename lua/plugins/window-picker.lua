cc1b67a0ec3777def375017bec6e01446a6405d8
moyiz <8603313+moyiz@users.noreply.github.com>
2024-04-14 17:15:06 +0300

    Use window picker with leader+, and exclude current window

--- lua/plugins/window-picker.lua
+++ lua/plugins/window-picker.lua
@@ -3,8 +3,24 @@ return {
   name = "window-picker",
   event = "VeryLazy",
   version = "2.*",
+  keys = {
+    {
+      "<leader>,",
+      function()
+        local win_id = require("window-picker").pick_window()
+        if win_id ~= nil then
+          vim.fn.win_gotoid(win_id)
+        end
+      end,
+      mode = "n",
+      desc = "Window picker",
+    },
+  },
   opts = {
     hint = "floating-big-letter",
     selection_chars = "tnresaluc,io",
+    filter_rules = {
+      include_current_win = false,
+    },
   },
 }
