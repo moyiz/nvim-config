a65a68c74d8f5c6d9f75413bb32528adad0cb275
moyiz <8603313+moyiz@users.noreply.github.com>
2024-04-14 17:15:52 +0300

    Add git-dev.nvim

---
+++ lua/plugins/git-dev.lua
@@ -0,0 +1,26 @@
+return {
+  "moyiz/git-dev.nvim",
+  -- dir = "~/workspace/git-dev.nvim",
+  event = "VeryLazy",
+  keys = {
+    {
+      "<leader>o",
+      function()
+        local repo = vim.fn.input "Repository name / URI: "
+        if repo ~= "" then
+          require("git-dev").open(repo)
+        end
+      end,
+      mode = "n",
+      desc = "[O]pen a remote git repository",
+    },
+  },
+  opts = {
+    cd_type = "tab",
+    opener = function(dir)
+      vim.cmd "tabnew"
+      vim.cmd("NvimTreeOpen " .. vim.fn.fnameescape(dir))
+    end,
+    -- verbose = true,
+  },
+}
