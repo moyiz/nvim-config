7c2533a7287f4bbe3f55715d27b8c45ee6d802a8
moyiz <8603313+moyiz@users.noreply.github.com>
2024-04-15 15:12:49 +0300

    mini.starter: consistent path view and add pwd. Add mini.diff

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -35,14 +35,16 @@ return {
       },
     }
 
-    -- Inserts closing character for pairs
-    -- require("mini.pairs").setup {
-    --   modes = {
-    --     command = true,
-    --   },
-    -- }
+    -- Diff
+    require("mini.diff").setup()
+    vim.keymap.set(
+      "n",
+      "<leader>gd",
+      require("mini.diff").toggle_overlay,
+      { desc = "Show inline [D]iff" }
+    )
 
-    --  overview of current buffer text.
+    -- overview of current buffer text.
     require("mini.map").setup()
     vim.keymap.set(
       "n",
@@ -131,7 +133,7 @@ return {
       local items = {}
       for _, w in pairs(workspaces.get()) do
         table.insert(items, {
-          name = w.name .. " " .. w.path,
+          name = w.name .. " " .. vim.fn.fnamemodify(w.path, ":~:."),
           action = "WorkspacesOpen " .. w.name,
           section = "Workspaces",
         })
@@ -140,43 +142,54 @@ return {
     end
 
     -- A workaround to centralize everything.
-    -- `aligning` will center all content according to the longest line. It
-    -- causes the header to not be truly centralized.
-    -- It also goes over workspaces and recent files, and "justifies" them
-    -- (splitting by space).
-    local centralize = function(content, buf_id)
-      -- Get max line width, same as in `aligning`
-      local max_line_width = math.max(unpack(vim.tbl_map(function(l)
-        return vim.fn.strdisplaywidth(l)
-      end, starter.content_to_lines(content))))
+    -- `aligning("center", "center")` will centralize the longest line in
+    -- `content`, then left align other items to its beginning.
+    -- It causes the header to not be truly centralized and have a variable
+    -- shift to the left.
+    -- This function will use `aligning` and pad the header accordingly.
+    -- It also goes over `justified_sections`, goes over all their items names
+    -- and justifies them by padding existing space in them.
+    local centralize = function(justified_sections, centralize_header)
+      return function(content, buf_id)
+        -- Get max line width, same as in `aligning`
+        local max_line_width = math.max(unpack(vim.tbl_map(function(l)
+          return vim.fn.strdisplaywidth(l)
+        end, starter.content_to_lines(content))))
 
-      -- Align
-      content = starter.gen_hook.aligning("center", "center")(content, buf_id)
+        -- Align
+        content = starter.gen_hook.aligning("center", "center")(content, buf_id)
 
-      -- Iterate over header items and pad with relative missing spaces
-      local coords = starter.content_coords(content, "header")
-      for _, c in ipairs(coords) do
-        local unit = content[c.line][c.unit]
-        local pad = (max_line_width - vim.fn.strdisplaywidth(unit.string)) / 2
-        if unit.string ~= "" then
-          unit.string = string.rep(" ", pad) .. unit.string
+        -- Iterate over header items and pad with relative missing spaces
+        if centralize_header == true then
+          local coords = starter.content_coords(content, "header")
+          for _, c in ipairs(coords) do
+            local unit = content[c.line][c.unit]
+            local pad = (max_line_width - vim.fn.strdisplaywidth(unit.string))
+              / 2
+            if unit.string ~= "" then
+              unit.string = string.rep(" ", pad) .. unit.string
+            end
+          end
         end
-      end
 
-      -- Justify recent files and workspaces
-      coords = starter.content_coords(content, "item")
-      for _, c in ipairs(coords) do
-        local unit = content[c.line][c.unit]
-        if
-          vim.tbl_contains({ "Recent files", "Workspaces" }, unit.item.section)
-        then
-          local one, two = unpack(vim.split(unit.string, " "))
-          unit.string = one
-            .. string.rep(" ", max_line_width - vim.fn.strlen(unit.string) + 1)
-            .. two
+        -- Justify recent files and workspaces
+        if justified_sections ~= nil and #justified_sections > 0 then
+          local coords = starter.content_coords(content, "item")
+          for _, c in ipairs(coords) do
+            local unit = content[c.line][c.unit]
+            if vim.tbl_contains(justified_sections, unit.item.section) then
+              local one, two = unpack(vim.split(unit.string, " "))
+              unit.string = one
+                .. string.rep(
+                  " ",
+                  max_line_width - vim.fn.strdisplaywidth(unit.string) + 1
+                )
+                .. two
+            end
+          end
         end
+        return content
       end
-      return content
     end
 
     starter.setup {
@@ -198,18 +211,23 @@ return {
         .. "████▌░░░▀┬┼┼┼┼┼┼┼┬▀░░░▐████\n"
         .. "█████▄░░░└┴┴┴┴┴┴┴┘░░░▄█████\n"
         .. "███████▄░░░░░░░░░░░▄███████\n"
-        .. "██████████▄▄▄▄▄▄▄██████████\n",
+        .. "██████████▄▄▄▄▄▄▄██████████\n"
+        .. "pwd: "
+        .. vim.fn.fnamemodify(vim.fn.getcwd(), ":~:."),
 
       items = {
         workspace_items,
-        starter.sections.recent_files(10, false),
+        starter.sections.recent_files(10, false, function(path)
+          -- Bring back trailing slash after `dirname`
+          return " " .. vim.fn.fnamemodify(path, ":~:.:h") .. "/"
+        end),
         { section = "Tools", name = "Lazy", action = "Lazy" },
         { section = "Tools", name = "Telescope", action = "Telescope" },
         starter.sections.builtin_actions(),
       },
       content_hooks = {
-        centralize,
-        starter.gen_hook.adding_bullet(),
+        -- starter.gen_hook.adding_bullet(),
+        centralize({ "Recent files", "Workspaces" }, true),
       },
     }
 
