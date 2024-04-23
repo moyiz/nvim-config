576be5e0778bd99f8b7b157c8fe3eccad1f87f4d
moyiz <8603313+moyiz@users.noreply.github.com>
2024-04-23 16:20:34 +0300

    lsp: add checkmake, shellcheck, prettier, yamlls, actionlint, terraformls, tflint, hclfmt

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -180,25 +180,17 @@ return {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         bashls = {},
-        yamlls = {},
+        shellcheck = {},
+
+        checkmake = {},
+
         clangd = {},
         gopls = {},
         pyright = {},
         -- pylyzer = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
 
+        stylua = {},
         lua_ls = {
-          -- cmd = {...},
-          -- filetypes { ...},
-          -- capabilities = {},
           settings = {
             Lua = {
               runtime = { version = "LuaJIT" },
@@ -218,6 +210,39 @@ return {
             },
           },
         },
+
+        prettier = {},
+        yamlls = {
+          on_attach = function(client, _)
+            -- Formatting support needs to be forcibly enabled
+            client.server_capabilities.documentFormattingProvider = true
+          end,
+          settings = {
+            editor = {
+              tabSize = 2,
+            },
+            yaml = {
+              format = {
+                enable = true,
+                singleQuote = false,
+              },
+              validate = true,
+              completion = true,
+            },
+            schemaStore = {
+              enable = true,
+            },
+            schemas = {
+              ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+            },
+          },
+          redhat = {
+            telemetry = {
+              enabled = false,
+            },
+          },
+        },
+        actionlint = {},
         helm_ls = {
           ["helm-ls"] = {
             yamlls = {
@@ -225,24 +250,15 @@ return {
             },
           },
         },
+        terraformls = {},
+        tflint = {},
+        hclfmt = {},
       }
 
       -- Ensure the servers and tools above are installed
-      --  To check the current status of installed tools and/or manually install
-      --  other tools, you can run
-      --    :Mason
-      --
-      --  You can press `g?` for help in this menu
       require("mason").setup()
-
-      -- You can add other tools here that you want Mason to install
-      -- for you, so that they are available from within Neovim.
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        "stylua", -- Used to format lua code
-      })
       require("mason-tool-installer").setup {
-        ensure_installed = ensure_installed,
+        ensure_installed = vim.tbl_keys(servers or {}),
       }
 
       require("mason-lspconfig").setup {
@@ -262,6 +278,7 @@ return {
                 capabilities,
                 server.capabilities or {}
               ),
+              on_attach = server.on_attach,
             }
           end,
         },
