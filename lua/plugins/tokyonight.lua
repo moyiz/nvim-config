334daa9686aaae3cfbd756c781d94f8a2dacdd4d
moyiz <8603313+moyiz@users.noreply.github.com>
2024-05-23 14:06:28 +0300

    tokyonight: more visible color for unused code

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,10 +1,7 @@
 return {
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
+  {
     "folke/tokyonight.nvim",
+    -- enabled = false,
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
@@ -14,13 +11,22 @@ return {
           sidebars = "transparent",
           floats = "transparent",
         },
+        on_highlights = function(hl, c)
+          hl.Comment = {
+            style = {
+              italic = true,
+            },
+            fg = "#207ffc",
+          }
+          hl.DiagnosticUnnecessary = {
+            style = {
+              underline = true,
+            },
+            fg = "#207ffc",
+          }
+        end,
       }
-      -- Load the colorscheme here
       vim.cmd.colorscheme "tokyonight-storm"
-
-      -- You can configure highlights by doing something like
-      vim.cmd.hi "Comment cterm=italic guifg=#207ffc"
     end,
   },
 }
--- vim: ts=2 sts=2 sw=2 et
