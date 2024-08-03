5c46308bc109f13f38a0a99113b1f6d38007d6aa
moyiz <8603313+moyiz@users.noreply.github.com>
2024-08-03 16:00:44 +0300

    conform: add Format{Disable,Enable} commands

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,10 +4,13 @@ return {
     -- enabled = false,
     opts = {
       notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_format = "fallback" }
+      end,
       formatters_by_ft = {
         lua = { "stylua" },
         go = { "goimports", "gofumpt" },
