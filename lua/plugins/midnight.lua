485a2389b3289b96ed29ff2439fb7ca4d6745750
moyiz <8603313+moyiz@users.noreply.github.com>
2024-09-02 21:49:03 +0300

    midnight: add some mini.starter and mini.statusline hlgroups

--- lua/plugins/midnight.lua
+++ lua/plugins/midnight.lua
@@ -4,9 +4,43 @@ return {
   priority = 1000,
   config = function()
     local colors = require "midnight.colors"
-    -- colors.palette.black = "#000000"
-    colors.components.bg = "#000000"
-    require("midnight").setup()
+    local p = colors.palette
+    local c = colors.components
+
+    c.bg = "#000000"
+
+    require("midnight").setup {
+      MiniStarterSection = {
+        link = "Keyword",
+      },
+      MiniStarterFooter = {
+        link = "Include",
+      },
+      MiniStarterItemPrefix = {
+        link = "Parameter",
+      },
+      -- MiniStarterItem = {
+      --   link = "Function",
+      -- },
+      MiniStatuslineModeCommand = {
+        fg = p.black,
+        bg = p.yellow[1],
+        bold = true,
+      },
+      -- MiniStatuslineDevinfo = { fg = c.fg, bg = c.fg },
+      -- MiniStatuslineFileinfo = { fg = c.fg, bg = c.fg },
+      -- MiniStatuslineFilename = { fg = c.fg, bg = p.bg },
+      -- MiniStatuslineInactive = { fg = p.blue[1], bg = p.bg_statusline },
+      MiniStatuslineModeInsert = { fg = p.black, bg = p.green[3], bold = true },
+      MiniStatuslineModeNormal = { fg = p.black, bg = p.blue[4], bold = true },
+      MiniStatuslineModeOther = { fg = p.black, bg = p.teal[1], bold = true },
+      MiniStatuslineModeReplace = { fg = p.black, bg = p.red[3], bold = true },
+      MiniStatuslineModeVisual = {
+        fg = p.black,
+        bg = p.magenta[2],
+        bold = true,
+      },
+    }
     vim.cmd.colorscheme "midnight"
   end,
 }
