2e7d852463f491210a27dbc77a0b9b4f67a7f963
moyiz <8603313+moyiz@users.noreply.github.com>
2024-10-12 00:31:40 +0300

    lsp: fix hover keymap

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -161,7 +161,7 @@ return {
 
           -- Opens a popup that displays documentation about the word under your cursor
           --  See `:help K` for why this keymap
-          map("K", vim.lsp.buf.hover(), "Hover Documentation")
+          map("K", vim.lsp.buf.hover, "Hover Documentation")
 
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
