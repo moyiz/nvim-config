617d8e3fa331f07ced99de660052ec99a3759ad5
moyiz <8603313+moyiz@users.noreply.github.com>
2024-12-16 19:57:27 +0200

    conform: prettier for css and html

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -45,6 +45,8 @@ return {
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
+        html = { "prettier" },
+        css = { "prettier" },
       },
     },
   },
