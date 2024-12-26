1331f18a51b6fb3b3a438f1e85da7154f94b3a1d
moyiz <8603313+moyiz@users.noreply.github.com>
2024-12-26 13:41:59 +0200

    conform: prettier javascript

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -42,9 +42,7 @@ return {
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { "prettier" },
         html = { "prettier" },
         css = { "prettier" },
       },
