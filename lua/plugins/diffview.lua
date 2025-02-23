f23a5e9f9a577584e9efd4dc7596a1376ba7a704
moyiz <8603313+moyiz@users.noreply.github.com>
2025-02-23 11:20:00 +0200

    diffview: add

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,14 @@
+return {
+  "sindrets/diffview.nvim",
+  lazy = true,
+  cmd = {
+    "DiffviewClose",
+    "DiffviewFileHistory",
+    "DiffviewFocusFiles",
+    "DiffviewLog",
+    "DiffviewOpen",
+    "DiffviewRefresh",
+    "DiffviewToggleFiles",
+  },
+  opts = {},
+}
