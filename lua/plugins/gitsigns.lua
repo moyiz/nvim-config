e2b15995e61089c25103a713dbfadddfb5a83ce3
moyiz <8603313+moyiz@users.noreply.github.com>
2025-03-13 16:35:26 +0200

    gitsigns: current line blame and blame view

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,12 +3,21 @@ return {
     "lewis6991/gitsigns.nvim",
     event = "VeryLazy",
     opts = {
+      current_line_blame = true,
       signs = {
         add = { text = "+" },
         change = { text = "~" },
         delete = { text = "_" },
         topdelete = { text = "‾" },
         changedelete = { text = "~" },
+      },
+    },
+    key = {
+      {
+        "<leader>ga",
+        "<Cmd>Gitsigns blame",
+        mode = { "n", "v" },
+        desc = "[G]it Bl[a]me",
       },
     },
   },
