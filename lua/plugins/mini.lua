f48f85334e0d954897d081ec8e0e3c97d14633cb
moyiz <8603313+moyiz@users.noreply.github.com>
2025-06-14 23:57:38 +0300

    mini.ai: treesitter text objects

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -7,7 +7,24 @@ return {
     --  - va)  - [V]isually select [A]round [)]parenthen
     --  - yinq - [Y]ank [I]nside [N]ext [']quote
     --  - ci'  - [C]hange [I]nside [']quote
-    require("mini.ai").setup { n_lines = 500 }
+    local ai = require "mini.ai"
+    ai.setup {
+      n_lines = 500,
+      custom_textobjects = {
+        o = ai.gen_spec.treesitter({
+          a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+          i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+        }, {}),
+        f = ai.gen_spec.treesitter(
+          { a = "@function.outer", i = "@function.inner" },
+          {}
+        ),
+        c = ai.gen_spec.treesitter(
+          { a = "@class.outer", i = "@class.inner" },
+          {}
+        ),
+      },
+    }
 
     require("mini.align").setup {}
 
