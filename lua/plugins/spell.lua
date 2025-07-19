4bcae45f1997e62bf9c75842132eb1b38d741332
moyiz <8603313+moyiz@users.noreply.github.com>
2025-07-20 00:26:47 +0300

    spell: add site path

--- lua/plugins/spell.lua
+++ lua/plugins/spell.lua
@@ -4,6 +4,7 @@ return {
     build = ":DirtytalkUpdate",
     config = function()
       vim.opt.spelllang = { "en", "programming" }
+      vim.opt.rtp:append(vim.fn.stdpath "data" .. "/site")
     end,
   },
 }
