fad3510cca28f178010c66fb6163bac5907b991c
moyiz <8603313+moyiz@users.noreply.github.com>
2026-04-06 11:54:06 +0300

    colorizer: default

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -3,7 +3,7 @@ return {
   event = "VeryLazy",
   config = function()
     -- #00f #fff #00f
-    require("colorizer").setup { "*" }
+    require("colorizer").setup {}
     require("colorizer").attach_to_buffer()
   end,
 }
